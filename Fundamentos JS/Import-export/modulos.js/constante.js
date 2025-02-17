e30296cbaf8be630f3135c61711a442abb728a2a
Gustavo_Garcia <garciapino.gus@gmail.com>
2025-02-17 10:44:19 -0300

    Update JS

---
+++ Fundamentos JS/Import-export/modulos.js/constante.js
@@ -0,0 +1,9 @@
+export const PI = Math.PI;
+
+export let usuario = "Gustavo";
+export let password = "qwerty";
+
+// Solo puedes tener 1 exportación por default (defecto)
+export default function saludar() {
+    console.log("Hola modulos + ES6");
+}
---
+++ Fundamentos JS/Import-export/modulos.js/modulos.js
@@ -0,0 +1,8 @@
+import saludar, {PI, usuario, password} from "./constante.js"
+import {aritmetica} from "./aritmetica.js";
+
+console.log("Archivo Modulos.JS");
+console.log(PI, usuario, password);
+console.log(aritmetica.sumar(2, 3));
+console.log(aritmetica.restar(10, 6));
+saludar();
