10e1cdada1ddfb4dd695a70c110d457cdb393059
Gustavo_Garcia <garciapino.gus@gmail.com>
2025-03-23 12:42:25 -0300

    Update

--- Estudios JavaScript/Programación Web/DOM-introducción/DOM.js
+++ Estudios JavaScript/Programación Web/DOM-introducción/DOM.js
@@ -8,7 +8,7 @@ speak(new SpeechSynthesisUtterance(texto));
 
 hablar(texto);
 */
-console.log("*********** Elementos del Documento **************");
+/*console.log("*********** Elementos del Documento **************");
 //console.log(window.document);
 console.log(document);
 console.log(document.head);
@@ -26,4 +26,15 @@ setTimeout(() => {
     console.log(document.getSelection().toString());
 }, 3000);
 document.write("<h2>Hola mundo desde el DOM<h2>")
+*/
 
+console.log(document.getElementsByTagName("li"));
+console.log(document.getElementsByClassName("card"));
+console.log(document.getElementsByName("nombre"));
+console.log(document.getElementById("menu"));
+console.log(document.querySelector("#menu"));
+console.log(document.querySelectorAll("a"));
+console.log(document.querySelectorAll("a").length);
+document.querySelectorAll("a").forEach((el) => console.log(el));
+console.log(document.querySelectorAll(".card"));
+console.log(document.querySelectorAll(".card")[1]);
